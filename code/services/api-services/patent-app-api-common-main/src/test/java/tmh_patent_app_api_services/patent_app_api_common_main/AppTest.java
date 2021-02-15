169e97810441814e132c2cc48bec0fb3ebba0369
Training My Hobby <33324108+TrainingMyHobby@users.noreply.github.com>
2021-02-15 16:45:45 -0500

    Initial Commit

---
+++ code/services/api-services/patent-app-api-common-main/src/test/java/tmh_patent_app_api_services/patent_app_api_common_main/AppTest.java
@@ -0,0 +1,20 @@
+package tmh_patent_app_api_services.patent_app_api_common_main;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
