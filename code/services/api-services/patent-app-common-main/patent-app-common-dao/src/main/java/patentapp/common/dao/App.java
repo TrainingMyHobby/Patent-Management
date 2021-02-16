dc6c18c8fb99a482cf3c2e75b66c6f0882f01259
Training My Hobby <33324108+TrainingMyHobby@users.noreply.github.com>
2021-02-15 22:57:27 -0500

    Initial version of patentapp-app-common

---
+++ code/services/api-services/patent-app-common-main/patent-app-common-dao/src/main/java/patentapp/common/dao/App.java
@@ -0,0 +1,13 @@
+package patentapp.common.dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ code/services/api-services/patent-app-common-main/patent-app-common-services/src/main/java/patentapp/common/services/App.java
@@ -0,0 +1,13 @@
+package patentapp.common.services;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
